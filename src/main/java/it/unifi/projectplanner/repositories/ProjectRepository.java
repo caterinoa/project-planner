64adb21acc9b4f98dbf5743d5701bd41978bd112
caterina <caterina@caterina-kubuntu>
2021-03-06 03:39:16 +0100

    Added coverage exclusion and some fixes

--- src/main/java/it/unifi/projectplanner/repositories/ProjectRepository.java
+++ src/main/java/it/unifi/projectplanner/repositories/ProjectRepository.java
@@ -2,12 +2,17 @@ package it.unifi.projectplanner.repositories;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import it.unifi.projectplanner.model.Project;
 
+@Repository
 public class ProjectRepository {
 
+	private static final String TEMPORARY_IMPLEMENTATION = "Temporary implementation";
+	
 	public List<Project> findAll() {
-		return null;
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
 	}
 
 }
--- src/main/java/it/unifi/projectplanner/services/ProjectService.java
+++ src/main/java/it/unifi/projectplanner/services/ProjectService.java
@@ -2,11 +2,16 @@ package it.unifi.projectplanner.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import it.unifi.projectplanner.model.Project;
 import it.unifi.projectplanner.repositories.ProjectRepository;
 
+@Service
 public class ProjectService {
 
+	@Autowired
 	private ProjectRepository projectRepository;
 	
 	public ProjectService(ProjectRepository projectRepository) {
