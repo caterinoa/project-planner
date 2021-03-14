e84dcbe2bfd8fd5ec655935a3eb6915bcbd25bfe
caterina <caterina@caterina-kubuntu>
2021-03-14 23:32:43 +0100

    Configured build for IT, e2e tests and mutation testing

--- src/bdd/java/it/unifi/projectplanner/bdd/steps/ButtonsSteps.java
+++ src/bdd/java/it/unifi/projectplanner/bdd/steps/ButtonsSteps.java
@@ -1,6 +1,5 @@
 package it.unifi.projectplanner.bdd.steps;
 
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.When;
 
 public class ButtonsSteps {
@@ -8,7 +7,7 @@ public class ButtonsSteps {
 	@When("The user clicks on the {string} button")
 	public void the_user_clicks_on_the_button(String string) {
 		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		throw new io.cucumber.java.PendingException();
 	}
 	
 }
--- src/bdd/java/it/unifi/projectplanner/bdd/steps/DatabaseSteps.java
+++ src/bdd/java/it/unifi/projectplanner/bdd/steps/DatabaseSteps.java
@@ -1,6 +1,5 @@
 package it.unifi.projectplanner.bdd.steps;
 
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
@@ -9,24 +8,24 @@ public class DatabaseSteps {
 	@Given("The database contains a few projects")
 	public void the_database_contains_a_few_projects() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    throw new io.cucumber.java.PendingException();
 	}
 	
 	@Given("The database contains a few tasks for a selected project")
 	public void the_database_contains_a_few_tasks_for_a_selected_project() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    throw new io.cucumber.java.PendingException();
 	}
 
 	@When("In the meantime the project has been removed from the database")
 	public void in_the_meantime_the_project_has_been_removed_from_the_database() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    throw new io.cucumber.java.PendingException();
 	}
 	
 	@When("In the meantime the task has been removed from the database")
 	public void in_the_meantime_the_task_has_been_removed_from_the_database() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    throw new io.cucumber.java.PendingException();
 	}
 }
