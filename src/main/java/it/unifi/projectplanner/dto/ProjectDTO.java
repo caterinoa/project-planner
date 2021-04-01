949aeda1f4ec23a1eb0471b132170db01f186342
caterina <caterina@caterina-kubuntu>
2021-04-01 10:43:14 +0200

    Added DTO and exception classes, and exclusions to pom.xml

---
+++ src/main/java/it/unifi/projectplanner/dto/ProjectDTO.java
@@ -0,0 +1,20 @@
+package it.unifi.projectplanner.dto;
+
+public class ProjectDTO {
+	
+	private String name;
+
+	public ProjectDTO() {
+		super();
+	}
+
+	public ProjectDTO(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
