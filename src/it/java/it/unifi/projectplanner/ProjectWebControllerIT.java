09324b7034f8e54904b964ebd2b85ee287b41524
caterina <caterina@caterina-kubuntu>
2021-04-18 20:16:49 +0200

    Web controller integration tests for the scenarios "Delete a project" and "Delete a not existing project"

--- src/it/java/it/unifi/projectplanner/ProjectWebControllerIT.java
+++ src/it/java/it/unifi/projectplanner/ProjectWebControllerIT.java
@@ -3,6 +3,7 @@ package it.unifi.projectplanner;
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.Optional;
 
@@ -70,5 +71,34 @@ class ProjectWebControllerIT {
 		Optional<Project> retrievedProject = projectRepository.findByName(NEW_PROJECT);
 		assertTrue(retrievedProject.isPresent());
 		assertThat(webDriver.findElement(By.id("projects_table")).getText()).contains(retrievedProject.get().getId().toString(), NEW_PROJECT, "0%");
+	}
+	
+	@Test
+	void test_newProjectWithExistingName_ShouldShowErrorMessage() {
+		projectRepository.save(new Project(SAVED_PROJECT, emptyList()));
+		
+		webDriver.get(baseURL);
+		webDriver.findElement(By.name("name")).sendKeys(SAVED_PROJECT);
+		webDriver.findElement(By.name("new_project_submit")).click();
+		
+		assertThat(webDriver.findElement(By.id("error")).getText()).isEqualTo("The specified name is already used for another project");
+	}
+	
+	@Test
+	void test_DeleteProject() {
+		Project saved = projectRepository.save(new Project(SAVED_PROJECT, emptyList()));
+		projectRepository.save(new Project("second project", emptyList()));
+		String id = saved.getId().toString();
+		webDriver.get(baseURL + "/delete/" + id);
+		
+		Optional<Project> retrievedProject = projectRepository.findByName(SAVED_PROJECT);
+		assertFalse(retrievedProject.isPresent());
+		assertThat(webDriver.findElement(By.id("projects_table")).getText()).doesNotContain(id, SAVED_PROJECT);
+	}
+	
+	@Test
+	void test_DeleteNonExistingProject_ShouldShowErrorMessage() {
+		webDriver.get(baseURL + "/delete/1");
+		assertThat(webDriver.findElement(By.id("error")).getText()).isEqualTo("The specified project does not exist");
 	}
 }
