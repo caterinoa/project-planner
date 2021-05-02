4d06144611c24fe06b7d50b8f3d5069e48649815
caterina <caterina@caterina-kubuntu>
2021-05-02 16:42:03 +0200

    Changed insertNewTaskIntoProject interface

--- src/test/java/it/unifi/projectplanner/services/ProjectServiceTest.java
+++ src/test/java/it/unifi/projectplanner/services/ProjectServiceTest.java
@@ -107,22 +107,21 @@ class ProjectServiceTest {
 
 	@Test
 	void test_InsertNewTaskIntoProject() {
-		Project toUpdate = spy(new Project(1L, SAVED_PROJECT_NAME, new ArrayList<>()));
-		Task savedToAdd = spy(new Task(1L, "to add", toUpdate));
-		toUpdate.addTask(savedToAdd);
+		Project projectToUpdate = spy(new Project(1L, SAVED_PROJECT_NAME, new ArrayList<>()));
+		Task taskToAdd = spy(new Task(1L, "to add", projectToUpdate));
+		projectToUpdate.addTask(taskToAdd);
 		
-		when(taskRepository.save(any(Task.class))).thenReturn(savedToAdd);
-		when(projectRepository.save(any(Project.class))).thenReturn(toUpdate);
+		when(taskRepository.save(any(Task.class))).thenReturn(taskToAdd);
+		when(projectRepository.save(any(Project.class))).thenReturn(projectToUpdate);
 		
-		Task toAdd = new Task("to add", toUpdate);
-		Project updated = projectService.insertNewTaskIntoProject(toAdd);
+		Task toAdd = new Task("to add", projectToUpdate);
+		Project updated = projectService.insertNewTaskIntoProject(toAdd, projectToUpdate);
 		
-		assertThat(updated).isSameAs(toUpdate);
-		InOrder inOrder = inOrder(toUpdate, savedToAdd, projectRepository, taskRepository);
+		assertThat(updated).isSameAs(projectToUpdate);
+		InOrder inOrder = inOrder(projectToUpdate, taskToAdd, projectRepository, taskRepository);
 		inOrder.verify(taskRepository, times(1)).save(toAdd);
-		inOrder.verify(savedToAdd, times(1)).getProject();
-		inOrder.verify(toUpdate, times(1)).addTask(savedToAdd);
-		inOrder.verify(projectRepository, times(1)).save(toUpdate);
+		inOrder.verify(projectToUpdate, times(1)).addTask(taskToAdd);
+		inOrder.verify(projectRepository, times(1)).save(projectToUpdate);
 	}
 	
 	@Test
