4cd50a48115b521305bb1dfddfc9bcabad265d67
caterina <caterina@caterina-kubuntu>
2021-05-03 23:45:35 +0200

    Web controller implementation for the deleting tasks scenarios

--- src/test/java/it/unifi/projectplanner/controllers/TaskWebControllerTest.java
+++ src/test/java/it/unifi/projectplanner/controllers/TaskWebControllerTest.java
@@ -2,8 +2,10 @@ package it.unifi.projectplanner.controllers;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,6 +23,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import it.unifi.projectplanner.exceptions.NonExistingProjectException;
+import it.unifi.projectplanner.exceptions.NonExistingTaskException;
 import it.unifi.projectplanner.model.Project;
 import it.unifi.projectplanner.model.Task;
 import it.unifi.projectplanner.services.ProjectService;
@@ -36,6 +39,8 @@ class TaskWebControllerTest {
 	private static final String PROJECT_ID_ATTRIBUTE = "project_id";
 	private static final String ERROR_ATTRIBUTE = "error";
 	private static final String MESSAGE_ATTRIBUTE = "message";
+	private static final String PROJECT_TASKS_ATTRIBUTE = "tasks";
+	private static final String PROJECTS_ATTRIBUTE = "projects";
 
 	@Autowired
 	private MockMvc mvc;
@@ -120,4 +125,40 @@ class TaskWebControllerTest {
 				
 		verify(projectService, times(0)).insertNewTaskIntoProject(new Task("new task", project), project);
 	}
+	
+	@Test
+	void test_DeleteTask_ByExistingTaskIdShouldDelete() throws Exception {
+		mvc.perform(get("/projectTasks/1/deletetask/1"))
+				.andExpect(view().name(REDIRECT_PROJECT_TASKS + "/1"));
+		
+		verify(taskService, times(1)).deleteTaskById(1L);
+	}
+	
+	@Test
+	void test_DeleteTask_ByNonExistingTaskIdShouldNotDelete() throws Exception {
+		Long taskId = 1L;
+		when(projectService.getProjectById(1L)).thenReturn(new Project(1L, "project", emptyList()));
+		doThrow(new NonExistingTaskException(taskId)).when(taskService).deleteTaskById(taskId);
+		
+		mvc.perform(get("/projectTasks/1/deletetask/1"))
+				.andExpect(view().name(PROJECT_TASKS))
+				.andExpect(model().attribute(ERROR_ATTRIBUTE, "The task with id=" + taskId + " does not exist"))
+				.andExpect(model().attribute(PROJECT_TASKS_ATTRIBUTE, emptyList()));
+		
+		verify(taskService, times(1)).deleteTaskById(1L);
+	}
+
+	@Test
+	void test_DeleteTask_OfNonExistingProjectShouldNotDelete() throws Exception {
+		Long projectId = 1L;
+		doThrow(new NonExistingProjectException(projectId)).when(projectService).getProjectById(projectId);
+
+		mvc.perform(get("/projectTasks/1/deletetask/1"))
+				.andExpect(view().name(INDEX))
+				.andExpect(model().attribute(ERROR_ATTRIBUTE, "The project with id=" + projectId + " does not exist"))
+				.andExpect(model().attribute(PROJECTS_ATTRIBUTE, emptyList()));
+
+		verifyNoInteractions(taskService);
+	}
+	
 }
